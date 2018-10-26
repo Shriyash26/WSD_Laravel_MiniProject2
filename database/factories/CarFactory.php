21721e59a0c89f2c8c6779e688f5e979fe4a6d68
shri <sm2689@njit.edu>
2018-10-26 14:38:22 -0400

    Fix: Fix for Error factory not found

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\car::class, function (Faker $faker) {
     return [
 
         'Make' => $faker->randomElement($array = array ('Ford','Honda','Toyota')),
