b57a79f99c41480629a824ed291eab52cc0c329e
shri <sm2689@njit.edu>
2018-10-27 18:20:12 -0400

    Fix: welcome page style fix 3

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -11,13 +11,13 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="//fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="//fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="../css/welcomePageStyles.css" rel="stylesheet">
+    <link href="css/welcomePageStyles.css" rel="stylesheet">
 
 </head>
