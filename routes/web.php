06bdd3445dd57bc409d51de2cefde4212c4d19a8
shri <sm2689@njit.edu>
2018-10-27 17:25:30 -0400

    Task: Added Routes for contact and thankyou page

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/about', 'AboutController@about')->name('about');
+Route::get('/contact','ContactController@contact')->name('contact');
+Route::post('/contact','ContactController@store')->name('contact.store');
+Route::get('/thankyou/{name}','ContactController@thankyou')->name('thankyou');
