67b452d621e7fd6bb9c4852db6330a3bcfad28ec
shri <sm2689@njit.edu>
2018-10-27 19:25:49 -0400

    Unit Test 4: Test to Count Records inserted by database seed

---
+++ tests/Unit/countNumberOfrecordsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+
+class countNumberOfrecordsTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = User::all();
+        $recordCount = $user->count();
+        $this->assertEquals(51, $recordCount);
+    }
+}
